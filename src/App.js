4e2af94e617a57625068c4aab207ae4faa5ea81d
hejo47 <hejo_47@naver.com>
2024-05-10 13:20:24 +0900

    feat: 초기화

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
 import "./css/style.scss";
-import Header from './components/common/Header';
-import Writers from './components/element/Writers';
 
 function App() {
   return (
     <div className="App">
       <div className="view">
-        <Header></Header>
-        <div className="content">
-          <Writers></Writers>
-        </div>
+
       </div>
     </div>
   );
