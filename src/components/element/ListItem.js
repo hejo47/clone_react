e63a67630bfbeb82e63b7de29bae92ec2640faf7
hejo47 <hejo_47@naver.com>
2024-05-16 18:01:25 +0900

    feat: axios 설정하기

--- src/components/element/ListItem.js
+++ src/components/element/ListItem.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 
-const ListItem = () => {
+const ListItem = (props) => {
   return (
     <li className="list__item">
       <Link to="#">
         <div className="list__item-img">
-          <img src="" alt="이미지" />
+          <img src={props.src} alt="이미지" />
         </div>
         <div className="list__item-txt">
-          <p className="title">title</p>
-          <p className="desc">desc desc desc desc desc desc desc desc desc desc desc desc desc desc</p>
+          <p className="title">{props.title}</p>
+          <p className="desc">{props.desc}</p>
         </div>
       </Link>
     </li>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,53 @@ import TitleWrap from "../element/TitleWrap";
 import ListItem from "../element/ListItem";
 import { Link } from "react-router-dom";
 
+
 const Array = [
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
+  {
+    src: "",
+    title: "이름",
+    desc: "설명"
+  },
   {
     src: "",
     title: "이름",
@@ -44,10 +90,48 @@ const Array = [
 const Home = () => {
   return (
     <Container>
-      <TitleWrap content="둘러보기" />
-      <ul className="list list-md">
-        <ListItem list={Array}/>
-      </ul>
+      <div className="list__container">
+        <div className="list__wrap">
+          <TitleWrap content="둘러보기" />
+          <ul className="list list-sm">
+            {
+              Array.slice(0, 5).map((item, idx) => (
+                <ListItem id={idx} src={item.src} title={item.title} desc={item.desc} />
+              ))
+            }
+          </ul>
+        </div>
+        <div className="list__wrap">
+          <TitleWrap content="추천 차트" />
+          <ul className="list list-md">
+            {
+              Array.slice(0, 7).map((item, idx) => (
+                <ListItem id={idx} src={item.src} title={item.title} desc={item.desc} />
+              ))
+            }
+          </ul>
+        </div>
+        <div className="list__wrap">
+          <TitleWrap content="인기 신곡" />
+          <ul className="list list-hr">
+            {
+              Array.map((item, idx) => (
+                <ListItem id={idx} src={item.src} title={item.title} desc={item.desc} />
+              ))
+            }
+          </ul>
+        </div>
+        <div className="list__wrap">
+          <TitleWrap content="취향에 맞는" />
+          <ul className="list list-lg">
+            {
+              Array.slice(0, 4).map((item, idx) => (
+                <ListItem id={idx} src={item.src} title={item.title} desc={item.desc} />
+              ))
+            }
+          </ul>
+        </div>
+      </div>
     </Container>
   )
 }
